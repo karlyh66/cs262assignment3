f6ec55438c4b377c86bd964eb7d84e753967fc87
karlyh66 <karlyh66@gmail.com>
2023-04-08 16:37:17 -0400

    listener thread for acknowledgements

--- server_t.cc
+++ server_t.cc
@@ -49,6 +49,17 @@ void sigabrtHandlerPrimary(int signum) {
     cout << "Interrupt signal (" << signum << ") received.\n";
 }
 
+void listen_for_acks(int backup_sd, int bytes_read, bool &received) {
+    while(1) {
+        // create a message buffer for the message to be received
+        char msg_recv[1500]; 
+        // reading from server
+        memset(&msg_recv, 0, sizeof(msg_recv)); // clear the buffer
+        received = true;
+        bytes_read += recv(backup_sd, (char*)&msg_recv, sizeof(msg_recv), 0);
+    }
+}
+
 void primary(int primarySd) {
     char msg[1500];
 
